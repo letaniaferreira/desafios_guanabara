2044380fdce9ac1e6659403efbe2c43aefd564c3
Letania Ferreira <letania@rollbar.com>
2020-09-04 19:42:03 -0500

    Desafio Brasileirao

---
+++ brasileirao.py
@@ -0,0 +1,5 @@
+brasileirao_2013=('cruzeiro', 'Gremio', 'Atletico', 'Botafogo', 'Vitoria', 'Goias', 'Santos', 'Atletico Mineiro')
+
+print('os 4 primeiro colocados foram {}'.format(brasileirao_2013[0:5]))
+print('os 4 ultimos colocados foram {}'.format(brasileirao_2013[-5:]))
+print('os times em ordem alfabetica sao {}'.format(sorted(brasileirao_2013[0:5])))
